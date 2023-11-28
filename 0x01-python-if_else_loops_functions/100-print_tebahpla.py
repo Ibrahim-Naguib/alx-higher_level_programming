73a89eb9b6d4e6f995b193b6f75c246ce0945ff6
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-11-28 14:51:40 +0000

    new commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i, j in zip(range(122, 96, -1), range(89, 64, -1)):
-    print("{0}{1}".format(chr(i), chr(j)), end="")
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
