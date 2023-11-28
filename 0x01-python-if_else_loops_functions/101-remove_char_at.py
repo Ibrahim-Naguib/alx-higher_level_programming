dbd793c9df62f2c4217bb32df0b89da2d2d898ef
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-11-28 14:28:39 +0000

    new commit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	copy = ""
-	for i, c in enumerate(str):
-		if i != n:
-			copy += c
-	return copy
+    copy = ""
+    for i, c in enumerate(str):
+        if i != n:
+            copy += c
+    return copy
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for c in str:
-		if ord(c) >= 97 and ord(c) <= 122:
-			c = chr(ord(c) - 32)
-			print(c, end="")
-		else:
-			print(c, end="")
-	print("")
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+            print(c, end="")
+        else:
+            print(c, end="")
+    print("")
