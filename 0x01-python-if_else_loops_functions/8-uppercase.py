73a26754cf34829db2fba7cbe3238fac0563eff7
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-11-28 14:46:37 +0000

    new commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-            print(c, end="")
-        else:
-            print(c, end="")
-    print("")
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
+
