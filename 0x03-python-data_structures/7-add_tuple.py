f14a50b870dfd5a622764d82534cec5df1281fee
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-12-04 07:23:19 +0000

    new commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    ta_n1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    ta_n2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    tb_n1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    tb_n2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (ta_n1 + ta_n2, tb_n1 + tb_n2)
