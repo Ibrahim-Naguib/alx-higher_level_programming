4fdedf34735ab484f1b3039201707ffe24fc5744
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-12-06 14:25:22 +0000

    new commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     keys = list(a_dictionary.keys())
-    for x in keys: 
+    for x in keys:
         if a_dictionary[x] == value:
             del a_dictionary[x]
     return a_dictionary
