f136390dad28fc76c812ee81e9a897d84d700d58
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-12-06 14:20:15 +0000

    new commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     num = 0
     for i in reversed(roman_string):
-        num = roman_nums[i]
+        num = nums[i]
         if total < num * 5:
             total += num
         else:
