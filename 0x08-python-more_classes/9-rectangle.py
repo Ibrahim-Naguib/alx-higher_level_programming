5fd8c57a7ed82ddded6d38be5a4293486c7fccae
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-01-04 19:09:21 +0000

    new commit

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
         """return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (self.__width * 2) + (self.__height * 2)
 
     @classmethod
     def square(cls, size=0):
