82783cf08986e79614be78b808df623b1c95f337
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-01-09 18:12:50 +0000

    new commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    item_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    item_list = []
-item_list.extend(sys.argv[1:])
-save_to_json_file(item_list, "add_item.json")
+    try:
+        item_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item_list = []
+    item_list.extend(sys.argv[1:])
+    save_to_json_file(item_list, "add_item.json")
