23a5b6e6c7b440be027dcc74e0dabd1e171827c2
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-11 11:30:29 +0000

    new commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length < 4) {
+if (argv.length <= 3) {
   console.log(0);
 } else {
   const list = argv.slice(2).map(Number).sort().reverse();
