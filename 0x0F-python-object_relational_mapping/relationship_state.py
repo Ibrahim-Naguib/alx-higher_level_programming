9bfbe641f406a27db917579d5c85142eb1a6b617
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-17 09:59:50 +0000

    new commit

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,8 +5,8 @@ Contains State class and Base, an instance of declarative_base()
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-meta = MetaData()
-Base = declarative_base(metadata=meta)
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
