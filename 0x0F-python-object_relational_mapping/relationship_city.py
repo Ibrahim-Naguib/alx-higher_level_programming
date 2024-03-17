5f1b778cb57e4f20f1c93b545a227df74f2abdee
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-17 09:41:40 +0000

    new commit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines City class"""
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base
 
 
 class City(Base):
