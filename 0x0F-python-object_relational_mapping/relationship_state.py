a09954cab7a3daf8cd04f6762647ce148469e077
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-17 09:49:52 +0000

    new commit

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Defines State class"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
