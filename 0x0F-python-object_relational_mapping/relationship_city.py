a89ccab82b0b553c3b027697d3ab56e26402ef63
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-17 09:43:41 +0000

    new commit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -15,6 +15,6 @@ class City(Base):
         state_id (int): The foreign key referencing the state.
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
