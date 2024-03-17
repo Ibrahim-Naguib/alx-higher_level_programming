092c9cb6727eb3dbae1b784a1cb941906372d043
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-17 09:38:27 +0000

    new commit

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-Base = declarative_base()
+meta = MetaData()
+Base = declarative_base(metadata=meta)
 
 
 class State(Base):
@@ -18,4 +19,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='states')
+    cities = relationship('City', backref='state')
