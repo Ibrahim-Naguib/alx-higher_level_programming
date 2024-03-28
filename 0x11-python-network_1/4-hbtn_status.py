c8234b373fdf475058915440b3ab43f1b1edf49d
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-28 20:53:47 +0000

    new commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
 
 
 if __name__ == '__main__':
-    import requests
 
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
